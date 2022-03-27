2d3d195dae6a3a37bac609cff5f359c2f0cf8943
HenCor2019 <henry200amaya@gmail.com>
2022-03-27 13:03:08 -0600

    :recycle: Refactor main configuration

--- Taller1/src/main/java/com/uca/taller1/SpringBootConfig.java
+++ Taller1/src/main/java/com/uca/taller1/SpringBootConfig.java
@@ -2,9 +2,18 @@ package com.uca.taller1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class SpringBootConfig {
+@ComponentScan
+public class SpringBootConfig extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+     return application.sources(SpringBootConfig.class);
+  }
 
  public static void main(String[] args) {
   SpringApplication.run(SpringBootConfig.class, args);
